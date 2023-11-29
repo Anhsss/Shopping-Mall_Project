fada972f4d41ba90f1eb09de4422e31dbb555f9c
Hyeonsoo An <ghost00xdg7@naver.com>
2023-11-30 02:01:06 +0900

    routes.tsx

--- shopping-mall/src/routes.tsx
+++ shopping-mall/src/routes.tsx
@@ -1,25 +1,30 @@
-//import { lazy } from "react";
-import GlobalLayout from "../src/pages/_layout";
-import Index from '../src/pages/index';
-import PoroductsIndex from '../src/pages/products/index';
-import ProductsId from '../src/pages/products/[id]';
+import GlobalLayout from './pages/_layout'
+import Index from './pages/index'
+import CartIndex from './pages/cart/index'
+import PaymentIndex from './pages/payment/index'
+import ProductsIndex from './pages/products/index'
+import ProductsId from './pages/products/[id]'
+// import AdminIndex from './pages/admin/index'
 
 export const routes = [
   {
-    path: "/",
+    path: '/',
     element: <GlobalLayout />,
     children: [
-      { path: "/", element: <Index />, index: true },
-      { path: "/products", element: <PoroductsIndex />, index: true },
-      { path: "/products/:id", element: <ProductsId />, index: true },
+      { path: '/', element: <Index />, index: true },
+      { path: '/cart', element: <CartIndex />, index: true },
+      { path: '/payment', element: <PaymentIndex />, index: true },
+      { path: '/products', element: <ProductsIndex />, index: true },
+      { path: '/products/:id', element: <ProductsId /> },
+      // { path: '/admin', element: <AdminIndex />, index: true },
     ],
   },
-];
+]
 
 export const pages = [
-  { route: "/" },
-  { route: "/cart" },
-  { route: "/payment" },
-  { route: "/products" },
-  { route: "/products/:id" },
-];
+  { route: '/' },
+  { route: '/cart' },
+  { route: '/payment' },
+  { route: '/products' },
+  { route: '/products/:id' },
+]
