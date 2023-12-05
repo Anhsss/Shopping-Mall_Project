cece99c81919e00647bfaeaf87c184ff29e66d58
Hyeonsoo An <ghost00xdg7@naver.com>
2023-12-06 02:06:04 +0900

    shopping

--- shopping-mall/server/src/index.ts
+++ shopping-mall/server/src/index.ts
@@ -1,36 +1,28 @@
-import express from "express";
-import { ApolloServer } from "apollo-server-express";
-import schema from "./schema";
-import resolvers from "./resolvers";
-// import env from './envLoader'
-import { DBField, readDB } from "./dbController";
-(async () => {
-  // const clientUrl = env.CLIENT_URL as string
-  // const port = env.PORT || 8000
+import express from 'express'
+import { ApolloServer } from 'apollo-server-express'
+import schema from './schema'
+import resolvers from './resolvers'
+import env from './envLoader'
+;(async () => {
+  const clientUrl = env.CLIENT_URL as string
+  const port = env.PORT || 8000
 
   const server = new ApolloServer({
     typeDefs: schema,
     resolvers,
-    context: {
-      db: {
-        prodcuts: readDB(DBField.PRODUCTS),
-        cart: readDB(DBField.CART),
-      },
-    },
-  });
+  })
 
-  const app = express();
-  await server.start();
+  const app = express()
+  await server.start()
   server.applyMiddleware({
     app,
-    path: "/graphql",
+    path: '/graphql',
     cors: {
-      origin: ["http://localhost:4000", "https://studio.apollographql.com"],
+      origin: [clientUrl, 'https://studio.apollographql.com'],
       credentials: true,
     },
-  });
-  await app.listen({ port: 8000 });
+  })
+  await app.listen({ port })
 
-  console.log("server listening on 8000...");
-  // console.log(readDB(DBField.PRODUCTS))
-})();
+  console.log(`server listening on ${port}...`)
+})()
